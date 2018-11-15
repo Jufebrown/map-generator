4381634acb659da1ce8552b7f2b4e08b1902532e
Jufebrown <jufebrown@gmail.com>
2018-11-15 09:13:43 -0600

    add determineStartingCell function

--- map-generator.js
+++ map-generator.js
@@ -1,19 +1,24 @@
 const generateBaseArray = (sizeX, sizeY) => {
   let baseArray = [];
-  const totalCells = sizeX * sizeY;
 
-  for (let cellNum = 0; cellNum < totalCells; cellNum++) {
-    for (let i = 0; i < sizeX; i++) {
-      for (let j = 0; j < sizeY; j++) {
-        baseArray[cellNum].xCoordinate = i;
-        baseArray[cellNum].yCoordinate = j;    
-      }
+  for (let i = 0; i < sizeX; i++) {
+    for (let j = 0; j < sizeY; j++) {
+      let cellObj = {};
+      cellObj.xCoordinate = i;
+      cellObj.yCoordinate = j;
+      cellObj.startingCell = false;
+      baseArray.push(cellObj);
     }
   }
-
   return baseArray;
 }
 
+const determineStartingCell = (baseArray) => {
+  startingCell = Math.floor(Math.random() * baseArray.length) + 1;
+  return startingCell;
+}
+
 const mapGenerator = (sizeX, sizeY) => {
+  const baseArray = generateBaseArray(sizeX, sizeY);
 
 }
