d7a10a2d20a1f6c24ab8518af18f179ad8b88b82
Jufebrown <jufebrown@gmail.com>
2018-11-16 14:21:50 -0600

    refactor to go back to array of objects

--- map-generator.js
+++ map-generator.js
@@ -1,32 +1,38 @@
+/* eslint-disable linebreak-style */
 // generate an array of cells based on given sizes
+
 const generateBaseMapArray = (sizeX, sizeY) => {
-  let mapArray = [];
-  let xArray = []
+  const mapArray = [];
 
-  for (let i = 0; i < sizeX; i++) {
-    let cellObj = {};
-    cellObj.isRoom = false;
-    cellObj.startingCell = false;
-    xArray.push(cellObj);
+  for (let i = 0; i < sizeX; i += 1) {
+    for (let j = 0; j < sizeY; j += 1) {
+      const cellObj = {};
+      cellObj.x = i;
+      cellObj.y = j;
+      cellObj.startingCell = false;
+      cellObj.cellType = 0;
+      mapArray.push(cellObj);
+    }
   }
 
-  for (let j = 0; j < sizeY; j++) {
-    mapArray.push(xArray);
-  }
 
   return mapArray;
-}
+};
 
-const randomNumberGenerator = (lowerLimit, upperLimit) {
-  return Math.floor(Math.random() * upperLimit) + lowerLimit;  
-}
+const randomNumGenerator = (loLimit, hiLimit) => Math.floor(Math.random() * (hiLimit)) + (loLimit);
 
 // pick one cell to be the starting position for the player
 const designateStartingCell = (sizeX, sizeY, mapArray) => {
-  const 
-}
+  // const xStartCoordinate = randomNumGenerator(0, sizeX);
+  // const yStartCoordinate = randomNumGenerator(0, sizeY);
+  // console.log('xstart: ', xStartCoordinate);
+  // console.log('ystart: ', yStartCoordinate);
+  const workingArray = mapArray;
+  workingArray[0][2] = 1;
+  return workingArray;
+};
 
 const mapGenerator = (sizeX, sizeY) => {
-  let mapArray = generateBaseMapArray(sizeX, sizeY);
+  const mapArray = generateBaseMapArray(sizeX, sizeY);
   return designateStartingCell(sizeX, sizeY, mapArray);
-}
+};
