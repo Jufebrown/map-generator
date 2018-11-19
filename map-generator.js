c4657330d89ab82a525e6e42f8a17addd6c53db8
Jufebrown <jufebrown@gmail.com>
2018-11-19 11:23:25 -0600

    bring back original functions

--- map-generator.js
+++ map-generator.js
@@ -1,6 +1,56 @@
 /* eslint-disable linebreak-style */
 // generate an array of cells based on given sizes
 
+const generateBaseMapArray = (sizeX, sizeY) => {
+  const mapArray = [];
+
+  for (let i = 0; i < sizeX; i += 1) {
+    for (let j = 0; j < sizeY; j += 1) {
+      const roomObj = {};
+      roomObj.x = i;
+      roomObj.y = j;
+      roomObj.startingCell = false;
+      roomObj.cellType = 0;
+      roomObj.doorNorth = 0;
+      roomObj.doorSouth = 0;
+      roomObj.doorEast = 0;
+      roomObj.doorWest = 0;
+      mapArray.push(roomObj);
+    }
+  }
+
+  return mapArray;
+};
+
+const isBoundaryCell = (mapArray, cellNumber, sizeX, sizeY) => {
+  if (mapArray[cellNumber].x === (0 || sizeX - 1)) {
+    return true;
+  }
+  if (mapArray[cellNumber].y === (0 || sizeY - 1)) {
+    return true;
+  }
+  return false;
+};
+
+const randomNumGenerator = (loLimit, hiLimit) => Math.floor(Math.random() * (hiLimit)) + (loLimit);
+
+// pick one cell to be the starting position for the player
+const designateStartingCell = (sizeX, sizeY, mapArray) => {
+  const workingArray = mapArray;
+  const numberOfCells = sizeX * sizeY;
+  const startingCell = workingArray[randomNumGenerator(0, numberOfCells)];
+  startingCell.startingCell = true;
+  startingCell.cellType = 1;
+  return workingArray;
+};
+
+const mapGenerator = (sizeX, sizeY) => {
+  let mapArray = generateBaseMapArray(sizeX, sizeY);
+  mapArray = designateStartingCell(sizeX, sizeY, mapArray);
+
+  return mapArray;
+};
+
 const drawMap = (sizeX, sizeY) => {
   let mapString = '';
   let rowString = '';
@@ -12,7 +62,6 @@ const drawMap = (sizeX, sizeY) => {
   for (let j = 0; j < sizeY; j += 1) {
     mapString += rowString;
   }
-  console.log('mapString', mapString);
   const miniMap = document.querySelector('.map');
   miniMap.innerHTML = mapString;
 };
