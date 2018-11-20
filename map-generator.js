37333958b3f0081d22c05adba24ba53f88a78977
Jufebrown <jufebrown@gmail.com>
2018-11-20 10:24:07 -0600

    add setPieces object

--- map-generator.js
+++ map-generator.js
@@ -3,6 +3,29 @@
 let startingCell = {};
 let currentCell = {};
 
+// cellTypes:
+//        0 = wall
+//        1 = room
+//        2 = exit
+
+const setPieces = [
+  {
+    name: 'exit',
+    description: 'There is a hole in the floor leading down...',
+    cellType: 2,
+  },
+  {
+    name: 'storage room',
+    description: 'Lots of stuff in here.',
+    cellType: 1,
+  },
+  {
+    name: 'other hole up',
+    description: 'There is another hole in the ceiling here',
+    cellType: 1,
+  },
+];
+
 const generateBaseMapArray = (sizeX, sizeY) => {
   const mapArray = new Array(sizeY);
 
