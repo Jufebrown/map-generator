f1f3f4ddd5a3ba930e28e7886011cd44c0209e4d
Jufebrown <jufebrown@gmail.com>
2018-11-20 16:20:59 -0600

    delete getLargerValue

--- src/getLargerValue.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable linebreak-style */
-
-export default function getLargerValue(value1, value2) {
-  const result1 = Math.abs(value1 - value2);
-  const result2 = Math.abs(value1 - 0);
-  return Math.max(result1, result2);
-}
