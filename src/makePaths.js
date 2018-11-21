608a425f0814e20e085c125a9667e0b8800bfa18
Jufebrown <jufebrown@gmail.com>
2018-11-21 11:28:52 -0600

    delete console.logs

--- src/makePaths.js
+++ src/makePaths.js
@@ -10,17 +10,13 @@ export default function makePaths(sizeX, sizeY, mapArray) {
   const startingCell = getStartingCell(mapArray);
   const direction = directions[randomNumberGenerator(0, 4)];
   let currentCell = startingCell;
-  const nextCell = getNeighborCellCoords(currentCell, direction);
+  let nextCell = getNeighborCellCoords(currentCell, direction);
   let workingArray = mapArray;
-
-  console.log('currentCell', currentCell);
-  console.log('direction', direction);
-  console.log('nextCell', nextCell);
 
   if (validCell(sizeX, sizeY, nextCell, workingArray)) {
     workingArray = assignDoor(currentCell, direction, mapArray);
     currentCell = nextCell;
-    // nextCell = getNeighborCellCoords(currentCell, direction);
+    nextCell = getNeighborCellCoords(currentCell, direction);
   }
   return workingArray;
 }
