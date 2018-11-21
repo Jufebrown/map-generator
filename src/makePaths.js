5d8661f53b18a42f07bff3dc924f3d4f29392e80
Jufebrown <jufebrown@gmail.com>
2018-11-21 14:49:30 -0600

    loop through setpieces and get displacement between them and exit

--- src/makePaths.js
+++ src/makePaths.js
@@ -7,19 +7,38 @@ import getDisplacementBetweenCells from './getDisplacementBetweenCells';
 import getSpecificSetPiece from './getSpecificSetPiece';
 import getAllSetPieces from './getAllSetPieces';
 
+const chooseAxis = () => {
+  const axes = ['x', 'y'];
+  const axis = axes[randomNumberGenerator(0, 2)];
+  return axis;
+};
+
 export default function makePaths(sizeX, sizeY, mapArray) {
   const directions = ['north', 'south', 'east', 'west'];
-  const startingCell = getSpecificSetPiece(mapArray, 2);
-  const exit = getSpecificSetPiece(mapArray, 3);
   const direction = directions[randomNumberGenerator(0, 4)];
-  let currentCell = startingCell;
-  let nextCell = getNeighborCellCoords(currentCell, direction);
   let workingArray = mapArray;
+  let axisOfMovement;
+  const displacementArray = [];
+
+  const exit = getSpecificSetPiece(mapArray, 3);
   const setPieceArray = getAllSetPieces(mapArray);
+  let currentCell = exit;
+  let nextCell = getNeighborCellCoords(currentCell, direction);
 
-
+  // loop through setpieces
   setPieceArray.array.forEach((setPiece) => {
+    // get displacement between exit and each setpiece
     const displacement = getDisplacementBetweenCells(exit, setPiece);
+    // choose the starting axis of direction to move on to make path
+    if (displacement.x !== 0 && displacement.y !== 0) {
+      axisOfMovement = chooseAxis();
+    } else if (displacement.x === 0 && displacement.y !== 0) {
+      axisOfMovement = 'y';
+    } else if (displacement.x !== 0 && displacement.y === 0) {
+      axisOfMovement = 'x';
+    } else {
+      axisOfMovement = null;
+    }
   });
 
   if (validCell(sizeX, sizeY, nextCell, workingArray)) {
