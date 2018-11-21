ce4412e59546c3e6560d72d907a74e4be0704ca4
Jufebrown <jufebrown@gmail.com>
2018-11-21 11:41:51 -0600

    change module name

---
+++ src/getDistanceBetweenCells.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+const difference = (value1, value2) => value1 - value2;
+
+export default function getDistanceBetweenSetPoints(cell1, cell2) {
+  const distance = {
+    x: difference(cell1.x, cell2.x),
+    y: difference(cell1.y, cell2.y),
+  };
+  return distance;
+}
