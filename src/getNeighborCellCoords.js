c02569ca2614b54a3708c9308b270195c3898c0b
Jufebrown <jufebrown@gmail.com>
2018-11-21 08:54:55 -0600

    refactor getNorthCellCoords to getNeighborCellCoords

---
+++ src/getNeighborCellCoords.js
@@ -0,0 +1,22 @@
+/* eslint-disable linebreak-style */
+export default function getNeighborCellCoords(currentCell, direction) {
+  const neighborCell = currentCell;
+
+  switch (direction) {
+    case 'north':
+      neighborCell.y += 1;
+      break;
+    case 'south':
+      neighborCell.y -= 1;
+      break;
+    case 'east':
+      neighborCell.x += 1;
+      break;
+    case 'west':
+      neighborCell.x -= 1;
+      break;
+    default:
+      break;
+  }
+  return neighborCell;
+}
--- src/getNorthCellCoords.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable linebreak-style */
-export default function (currentCell) {
-  const northCell = {
-    x: currentCell.x,
-    y: currentCell.y + 1,
-  };
-  return northCell;
-}
