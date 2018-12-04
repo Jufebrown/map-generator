1a8f18d44da77b2675654d97a480a21f15bb3aae
Jufebrown <jufebrown@gmail.com>
2018-12-04 13:49:30 -0600

    make logic for choosing starting axis it's own function

--- src/makePaths.js
+++ src/makePaths.js
@@ -13,6 +13,19 @@ const chooseRandomAxis = () => {
   return axis;
 };
 
+const determineStartingAxis = (displacement) => {
+  if (displacement.x !== 0 && displacement.y !== 0) {
+    return chooseRandomAxis();
+  }
+  if (displacement.x === 0 && displacement.y !== 0) {
+    return 'y';
+  }
+  if (displacement.x !== 0 && displacement.y === 0) {
+    return 'x';
+  }
+  return null;
+};
+
 export default function makePaths(sizeX, sizeY, mapArray) {
   const directions = ['north', 'south', 'east', 'west'];
   const direction = directions[randomNumberGenerator(0, 4)];
@@ -29,15 +42,8 @@ export default function makePaths(sizeX, sizeY, mapArray) {
     // get displacement between exit and each setpiece
     const displacement = getDisplacementBetweenCells(exit, setPiece);
     // choose the starting axis of direction to move on to make path
-    if (displacement.x !== 0 && displacement.y !== 0) {
-      axisOfMovement = chooseRandomAxis();
-    } else if (displacement.x === 0 && displacement.y !== 0) {
-      axisOfMovement = 'y';
-    } else if (displacement.x !== 0 && displacement.y === 0) {
-      axisOfMovement = 'x';
-    } else {
-      axisOfMovement = null;
-    }
+    axisOfMovement = determineStartingAxis(displacement);
+    // move down axis of movement setting each cell to be hallway and setting doors on each cell
   });
 
   if (validCell(sizeX, sizeY, nextCell, workingArray)) {
